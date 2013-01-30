95b91e17b3d5ca8504dcb4e3190b473bae6d4f15
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-01-30 10:35:19 -0800

    Making changes os the tests pass.

--- lib/stylesheets/whitespace.rb
+++ lib/stylesheets/whitespace.rb
@@ -1,18 +1,25 @@
 module StyleSheets
   module Whitespace
-    DEFAULT_INDENTATION_COUNT = 2
-    DEFAULT_INDENTATION_CHARACTER = " "
+    DEFAULT_INDENTATION_CHARACTER = "  "
+    DEFAULT_NEWLINE_CHARACTER = "\n"
 
-    def newline(allow = true)
-      allow ? "\n" : ""
+
+    def newline(compress = false)
+      whitespace compress, DEFAULT_NEWLINE_CHARACTER
     end
 
-    def indent(allow = true)
-      allow ? DEFAULT_INDENTATION_CHARACTER * DEFAULT_INDENTATION_COUNT : ""
+    def indent(compress = false)
+      whitespace compress, DEFAULT_INDENTATION_CHARACTER
     end
 
-    def space(allow = true)
-      allow ? " " : ""
+    def space(compress = false)
+      whitespace compress, " "
+    end
+
+    private
+
+    def whitespace(compress = false, with_whitespace)
+      compress ? "" : with_whitespace
     end
   end
 end
